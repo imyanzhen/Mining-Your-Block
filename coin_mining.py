8d3ee6c5b6b869c52eb9c7ddaa8d0c2536d3fea9
Yanzhen <34119752+imyanzhen@users.noreply.github.com>
2019-02-17 23:10:44 +0800

    Add files via upload

---
+++ coin_mining.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 30 00:05:45 2019
+
+@author: yanzhen
+"""
+
+import hashlib
+
+initstr = 'Alice sends 0.1 BTC to Bob.'
+#
+    #hash_object = hashlib.sha256(                ).encode())
+    hex_dig = hash_object.hexdigest()
+    #
+        
+        print('Found nonce: ' + str(nonce))
+        print('Hash: '+ hex_dig)
+        #
